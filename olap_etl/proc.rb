24284f344f446535701ee2a73349c4174a2f77b7
Hiroyuki Inoue <mammymax@gmail.com>
2011-12-26 10:38:58 +0900

    add proc.rb

--- olap_etl/proc.rb
+++ olap_etl/proc.rb
@@ -7,25 +7,18 @@ require 'pp'
 
 
 def get_location( uri )
-  
 end
-
 
 graph = RDF::Graph.load( "WSFO3_2005_8_26.n3" )
 
-
 graph.each_statement do |stm|
-#  puts stm.subject
-#  puts stm.predicate
-#  puts stm.predicate
-#  if stm.predicate.to_s == "om-owl:hasLocation"
-  if stm.predicate.to_s == "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation"
+  if stm.predicate.to_s == "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation" # "om-owl:hasLocation"
     begin
       uri = stm.object.to_s
       puts stm.object
 #      response = RestClient.get( uri << 'about.rdf'  )
 #      RDF::RDFXML::Reader.open( response ) do |reader|
-     RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader|
+     RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader| # RDFXML::Reader.open can open URI
         reader.each_statement do |statement|
           puts '\t' << statement.inspect
         end
@@ -34,8 +27,5 @@ graph.each_statement do |stm|
     rescue => evar
       puts 'GET failed'
     end
-#    pp response
-    p '=================================='
-    #  puts stm.object
   end
 end
