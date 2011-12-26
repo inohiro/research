e8ea93b414b0223fe8e02149061f62db8a9d2fc6
Hiroyuki Inoue <mammymax@gmail.com>
2011-12-26 10:57:36 +0900

    modified file path

--- olap_etl/proc_observatory.rb
+++ olap_etl/proc_observatory.rb
@@ -9,7 +9,9 @@ require 'pp'
 def get_location( uri )
 end
 
-graph = RDF::Graph.load( "WSFO3_2005_8_26.n3" )
+OBSERVATORY_PATH = "file:/Users/inohiro/Projects/rdf_rb/WSFO3_2005_8_26.n3"
+
+graph = RDF::Graph.load( OBSERVATORY_PATH )
 
 graph.each_statement do |stm|
   if stm.predicate.to_s == "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation" # "om-owl:hasLocation"
@@ -18,9 +20,10 @@ graph.each_statement do |stm|
       puts stm.object
 #      response = RestClient.get( uri << 'about.rdf'  )
 #      RDF::RDFXML::Reader.open( response ) do |reader|
-     RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader| # RDFXML::Reader.open can open URI
+      RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader| # RDFXML::Reader.open can open URI
         reader.each_statement do |statement|
-          puts '\t' << statement.inspect
+          puts statement.inspect
+#          puts '\t' << statement.inspect
         end
       end
       #    response = RestClient.get( stm.predicate.to_s )
