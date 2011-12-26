39eb540dfda353f1c34cc5e8d47a7e70cf110e53
Hiroyuki Inoue <mammymax@gmail.com>
2011-12-26 10:23:36 +0900

    separate dbpedia sample

--- geonames/rdf.rb
+++ geonames/rdf.rb
@@ -2,3 +2,10 @@ require 'linkeddata'
 require 'rdf'
 require 'sparql'
 
+base = 'http://sws.geonames.org/5606064/'
+rdf = base << "about.rdf"
+
+graph = RDF::Graph.load( rdf )
+puts graph.size
+puts graph.to_s
+
