a4bb6a83f65f200405e1b09bb38bc80f9167131d
Hiroyuki Inoue <mammymax@gmail.com>
2011-12-26 11:36:11 +0900

    extended get_location

--- olap_etl/proc_observatory.rb
+++ olap_etl/proc_observatory.rb
@@ -6,29 +6,40 @@ require 'rest-client'
 require 'pp'
 
 
+OBSERVATORY_PATH = "file:/Users/inohiro/Projects/rdf_rb/WSFO3_2005_8_26.n3"
+HAS_LOCATION = "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation" # "om-owl:hasLocation"
+PARENT_FEATURE = "http://www.geonames.org/ontology#parentFeature"
+GEONAMES_NAME = "http://www.geonames.org/ontology#name"
+
+LATITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#lat"
+LONGITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#long"
+ALTITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#alt"
+
 def get_location( uri )
+#      response = RestClient.get( uri << 'about.rdf'  )
+  RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader|
+    reader.each_statement do |stm|
+      if stm.predicate == PARENT_FEATURE
+        puts stm.inspect
+      elsif stm.predicate == GEONAMES_NAME
+        puts stm.inspect
+        # LATITUDE
+        # LONGITUDE
+        # ALTITUDE
+      end
+    end
+  end
 end
 
-OBSERVATORY_PATH = "file:/Users/inohiro/Projects/rdf_rb/WSFO3_2005_8_26.n3"
+observatory = RDF::Graph.load( OBSERVATORY_PATH )
 
-graph = RDF::Graph.load( OBSERVATORY_PATH )
-
-graph.each_statement do |stm|
-  if stm.predicate.to_s == "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation" # "om-owl:hasLocation"
+observatory.each_statement do |stm|
+  if stm.predicate.to_s == HAS_LOCATION
     begin
       uri = stm.object.to_s
-      puts stm.object
-#      response = RestClient.get( uri << 'about.rdf'  )
-#      RDF::RDFXML::Reader.open( response ) do |reader|
-      RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader| # RDFXML::Reader.open can open URI
-        reader.each_statement do |statement|
-          puts statement.inspect
-#          puts '\t' << statement.inspect
-        end
-      end
-      #    response = RestClient.get( stm.predicate.to_s )
+      get_location uri
     rescue => evar
-      puts 'GET failed'
+      puts 'HTTP GET failed'
     end
   end
 end
