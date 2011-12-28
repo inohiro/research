c0f977b792d8f552370cf8bb3129fad2eafc6991
Hiroyuki Inoue <mammymax@gmail.com>
2011-12-28 17:14:17 +0900

    use sequel o/r mapper

--- olap_etl/proc_observatory.rb
+++ olap_etl/proc_observatory.rb
@@ -1,53 +1,56 @@
+# -*- coding: utf-8 -*-
+
 require 'rdf'
 require 'rdf/n3'
 require 'rdf/rdfxml'
 
-# require 'rest-client'
-require 'mysql'
+require 'rubygems'
 require 'pp'
 require 'highline'
-
+require 'sequel'
 
 OBSERVATORY_PATH = "file:/Users/inohiro/Projects/rdf_rb/WSFO3_2005_8_26.n3"
 HAS_LOCATION = "http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#hasLocation" # "om-owl:hasLocation"
 PARENT_FEATURE = "http://www.geonames.org/ontology#parentFeature"
 GEONAMES_NAME = "http://www.geonames.org/ontology#name"
 
-LATITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#lat"
-LONGITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#long"
-ALTITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#alt"
+LATITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#lat"   # 緯度
+LONGITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#long" # 経度
+ALTITUDE = "http://www.w3.org/2003/01/geo/wgs84_pos#alt"   # 高度
+
+FOAF_PRIMARY_TOPIC = 'http://xmlns.com/foaf/0.1/primaryTopic'
+
 
 def get_location( uri )
 #      response = RestClient.get( uri << 'about.rdf'  )
-  insert( 'hoge' )
-  RDF::RDFXML::Reader.open( uri << 'about.rdf' ) do |reader|
+  RDF::RDFXML::Reader.open( uri + 'about.rdf' ) do |reader|
     reader.each_statement do |stm|
-      if stm.predicate == PARENT_FEATURE
-        puts stm.inspect
-      elsif stm.predicate == GEONAMES_NAME
-        puts stm.inspect
-        # LATITUDE
-        # LONGITUDE
-        # ALTITUDE
+      puts stm.inspect
+      case stm.predicate
+      when PARENT_FEATURE
+        #        get_location( stm.object.to_s )
+      when GEONAMES_NAME
+        puts stm.object
+      when LATITUDE
+      when LONGITUDE
+      when ALTITUDE
       end
     end
+#    insert( h )
   end
 end
 
 def insert( h )
-  @my.query( 'select * from common_countries order by id' ).each do |r|
-    pp r
-  end
+
 end
 
 def main
   host = HighLine.new.ask ( 'Hostname: ' )
   user = HighLine.new.ask( 'MySQL User: ' )
   passwd = HighLine.new.ask( 'MySQL Password: ' ) { |q| q.echo = '*'}
-  table = HighLine.new.ask( 'Table Name: ' )
+  dbname = HighLine.new.ask( 'Database Name: ' )
 
-  @my = Mysql.connect( host, user, passwd, table )
-  @my.charset = 'utf8'
+  @DB = Sequel.connect( "mysql://#{user}:#{passwd}@#{host}/#{dbname}", { :encoding => 'utf8' } )
 
   observatory = RDF::Graph.load( OBSERVATORY_PATH )
   observatory.each_statement do |stm|
@@ -61,7 +64,7 @@ def main
     end
   end
 
-  @my.close
+  @DB.disconnect
 end
 
 main
