4cfe63f73390c1beefa04aa613961ac567f3a417
Hiroyuki Inoue <mammymax@gmail.com>
2012-02-20 01:44:59 +0900

    wrote time rdf crawler

---
+++ crawler/ra/time.rb
@@ -0,0 +1,44 @@
+require 'rest_client'
+require 'rdf'
+require 'rdf/rdfxml'
+
+require 'pp'
+
+INPUT_DIR = './outputs/'
+TIME_DIR = INPUT_DIR + 'time/'
+TIME = "http://purl.org/NET/c4dm/event.owl#time"
+
+def main
+
+  unless File.exists?( INPUT_DIR )
+    return 1
+  end
+
+  unless File.exists?( TIME_DIR )
+    Dir.mkdir( TIME_DIR )
+  end
+
+  counter = 0
+
+  Dir.glob( INPUT_DIR + '*.rdf' ) do |f|
+    RDF::RDFXML::Reader.open( f ) do |reader|
+      reader.each do |stm|
+        if stm.predicate.to_s == TIME
+          begin
+            result = RestClient.get( stm.object.to_s, :timeout => 10 )
+            file_path = TIME_DIR + counter.to_s + '.rdf'
+            open( file_path, 'w' ) do |f|
+              f.write( result )
+            end
+          rescue => exp
+            pp exp
+          end
+          sec = Random.new.rand( 5..20 )
+          sleep( sec )
+        end
+      end
+    end
+  end
+end
+
+main
