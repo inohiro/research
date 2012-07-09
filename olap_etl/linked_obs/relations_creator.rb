4950a7c97763358013900b8b6ab85c17f56a25f7
Hiroyuki Inoue <mammymax@gmail.com>
2012-07-09 10:53:56 +0900

    remove vertical_* methods

--- olap_etl/linked_obs/relations_creator.rb
+++ olap_etl/linked_obs/relations_creator.rb
@@ -9,65 +9,6 @@ require 'uri'
 
 RELATION_INFOS_TABLE = :relation_infos
 
-def vertical_explorer( object, table_name )
-
-  puts "object: #{object.to_s}"
-  puts "table_name: #{table_name.to_s}"
-
-  table_list = @db[:uri_tablename].all
-  table_list.each do |t|
-    ft_table_name = ( 't' + t[:id].to_s ).to_sym
-    puts "current_table: #{ft_table_name}"
-    if table_name != ft_table_name
-      ft_all_subjects = @db[ft_table_name].select( :subject ).distinct
-
-
-      sampling = ft_all_subjects.first
-      sampling_domain = URI.parse( sampling[:subject] ).host
-
-      if sampling_domain == URI.parse( object ).host
-        ft_all_subjects.each do |ft_s|
-          ft_subject = ft_s[:subject]
-          if ft_subject == object
-            return ft_table_name
-          end
-        end
-      end
-    end
-  end
-  return nil #=> リソースだけどデータセットに含まれていない
-end
-
-def vertical_main
-  @db = Util.connect_db( { :db => 'test' } )
-
-  table_list = @db[:uri_tablename].all
-  table_list.each do |table|
-    unless table[:id] == 2 || table[:id] == 9
-
-      table_name = ( 't' + table[:id].to_s ).to_sym
-      puts "Table: #{table_name.to_s}"
-
-    a_subject = @db[table_name].select( :subject ).distinct.first
-    record = @db[table_name].filter( :subject => a_subject[:subject] )
-      record.each do |r|
-        if r[:value_type_id] == 1 && r[:predicate] != RDF.type
-          # さらに同一ホスト という条件を加える（正しい？
-          # まず1つ目だけ比較して，ホストが一緒か確認する．同じなら探す
-          object = r[:object]
-          result = vertical_explorer( object, table_name )
-          if result == nil
-            puts "relation not fount"
-          else
-            puts "result: #{result}"
-          end
-          puts
-        end
-      end
-    end
-  end
-end
-
 def recreate_table_with_relationships
   puts "recreate horizontal table with relationships".upcase
 
