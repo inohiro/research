55f43652e8b976a647127a0c5c7d53438563d57b
Hiroyuki Inoue(Riken) <mammymax@gmail.com>
2012-08-16 12:55:07 +0900

    add copy_table.rb

---
+++ olap_etl/biolod/copy_table.rb
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+require 'sequel'
+require './../util.rb'
+require 'pp'
+
+@src
+@dst
+
+def add_schema_info( table_name, attributes )
+  attributes.each do |a|
+    begin
+      @dst[:horizontal_infos].insert( :table_name => table_name.to_s,
+                                      :attribute_name => a[:column_name].to_s,
+                                      :data_type => a[:type].to_s,
+                                      :is_resource => a[:is_resource].to_s )
+    rescue => exp
+      puts "!!! unexpected insertion exception !!!".upcase
+      puts exp.message
+      puts exp.backtrace
+    end
+  end
+  puts "schema information were added to :horizontal_infos"
+end
+
+def create_table( table_name, attributes )
+  index_columns = []
+  index_columns << :subject
+
+  begin
+    @dst.create_table!( table_name, { :engine => 'innodb' } ) do
+      String :subject
+      attributes.each do |a|
+        column( a[:column_name], a[:type] )
+        if a[:is_resource] == true
+          index_columns << a[:column_name].to_sym
+        end
+      end
+    end
+  rescue => exp
+    puts "!!! unexoected create table exception !!!".upcase
+    puts exp.message
+    puts exp.backtrace
+  end
+  add_schema_info( table_name, attributes )
+  puts "new table: #{table_name.to_s} was created"
+  index_columns
+end
+
+def duplicate_data( src_table, dst_table )
+  puts 'data duplication are started'
+  src_table = src_table.to_sym
+  @src[src_table].each do |r|
+    begin
+      @dst[dst_table].insert( r )
+    rescue => exp
+      puts "!!! unexpected insertion exception !!!".upcase
+      puts exp.message
+      puts exp.backtrace
+    end
+  end
+  puts 'data duplication were completed'
+end
+
+def create_schema( src_table )
+  columns = @src[:horizontal_infos].filter( :table_name => src_table )
+  attributes = []
+  columns.each do |r|
+    attributes << { :type => Util.detect_data_type( r[:data_type] ),
+                    :column_name => r[:attribute_name],
+                    :is_resource => r[:is_resource] }
+  end
+  attributes
+end
+
+def print_help
+  puts <<EOS
+usage: ruby copy_table.db src_db src_table dst_db [table_name]
+EOS
+end
+
+def main( argv )
+  if argv.length < 3
+    ptinr_help
+  else
+    src_db = argv[0]
+    src_table = argv[1]
+    dst_db = argv[2]
+    table_name = ( argv.length == 4 ? argv[3] : "#{src_db}_#{src_table}" ).to_sym
+
+    begin
+      @src = Util.connect_db( { :db => src_db } )
+      @dst = Util.connect_db( { :db => dst_db } )
+    rescue => exp
+      puts "!!! unexpected connect db exception !!!".upcase
+      puts exp.message
+      puts exp.backtrace
+      return
+    end
+
+    # src_db からテーブル構造を読み取り、dst_db に作る
+    attributes = create_schema( src_table )
+    index_columns = create_table( table_name, attributes )
+    duplicate_data( src_table, table_name )
+    Util.add_index( @dst, table_name, index_columns )
+  end
+end
+
+main( ARGV )
